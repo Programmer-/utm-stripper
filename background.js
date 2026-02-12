bfc5b3d2c43cc03059a22fd5f176183700ae01b7
Justus Njuru <justnjuru@gmail.com>
2026-02-12 09:18:55 +0300

    Comment out webNavigation listener in background.js

--- background.js
+++ background.js
@@ -1,31 +1,31 @@
-chrome.webNavigation.onCompleted.addListener((details) => {
-  // Only run on main frame (the actual tab, not ads/iframes)
-  if (details.frameId === 0) {
-    const url = new URL(details.url);
-    const params = new URLSearchParams(url.search);
+// chrome.webNavigation.onCompleted.addListener((details) => {
+//   // Only run on main frame (the actual tab, not ads/iframes)
+//   if (details.frameId === 0) {
+//     const url = new URL(details.url);
+//     const params = new URLSearchParams(url.search);
     
-    // List of parameters to kill
-    const targets = ['utm_source', 'utm_medium', 'utm_campaign', 'utm_term', 'utm_content', 'fbclid', 'gclid'];
-    let removed = false;
+//     // List of parameters to kill
+//     const targets = ['utm_source', 'utm_medium', 'utm_campaign', 'utm_term', 'utm_content', 'fbclid', 'gclid'];
+//     let removed = false;
 
-    targets.forEach(param => {
-      if (params.has(param)) {
-        params.delete(param);
-        removed = true;
-      }
-    });
+//     targets.forEach(param => {
+//       if (params.has(param)) {
+//         params.delete(param);
+//         removed = true;
+//       }
+//     });
 
-    if (removed) {
-      const cleanURL = url.origin + url.pathname + (params.toString() ? '?' + params.toString() : '');
+//     if (removed) {
+//       const cleanURL = url.origin + url.pathname + (params.toString() ? '?' + params.toString() : '');
       
-      // Update the URL in the address bar without a full page refresh
-      chrome.scripting.executeScript({
-        target: { tabId: details.tabId },
-        func: (newUrl) => {
-          window.history.replaceState({}, '', newUrl);
-        },
-        args: [cleanURL]
-      });
-    }
-  }
-}, { url: [{ queryContains: 'utm_' }] });
+//       // Update the URL in the address bar without a full page refresh
+//       chrome.scripting.executeScript({
+//         target: { tabId: details.tabId },
+//         func: (newUrl) => {
+//           window.history.replaceState({}, '', newUrl);
+//         },
+//         args: [cleanURL]
+//       });
+//     }
+//   }
+// }, { url: [{ queryContains: 'utm_' }] });
